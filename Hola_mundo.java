b25bdcd5902607b089ec20ad8aa8fd3c5a72e036
Roberto Negro <rhonegro@gmail.com>
2022-04-24 21:03:41 +0200

    elimino saludos y añado hola Oviedo

--- Hola_mundo.java
+++ Hola_mundo.java
@@ -3,11 +3,8 @@ public class Hola_mundo {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola mundo!");
-		System.out.println("Hola jenkins!");
-		System.out.println("Hola Pepe!");
-		System.out.println("Hola Pepe!");
-		System.out.println("Hola marta!");
-		System.out.println("Hola juan!");
+		System.out.println("Hola Oviedo!");
+	
 
 	}
 
